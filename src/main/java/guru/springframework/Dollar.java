607d0528d31719514977a7bd0038b46c02b3ceb5
lentiummmx <lentium.mmx@gmail.com>
2021-03-31 17:33:46 -0600

    j-td-by-example File(s) committed to adding improvements (v0.1.0)

--- src/main/java/guru/springframework/Dollar.java
+++ src/main/java/guru/springframework/Dollar.java
@@ -15,4 +15,15 @@ public class Dollar {
         // this.amount *= multiplier;
         return new Dollar(amount * multiplier);
     }
+
+    public boolean equals(Object o) {
+        if (o == null) {
+            return false;
+        }
+        if (!(o instanceof Dollar)) {
+            return false;
+        }
+        Dollar other = (Dollar) o;
+        return amount == other.amount;
+    }
 }
