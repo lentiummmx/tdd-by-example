6f7466ad1676dd625d980753d3eb81aa9e89f0ae
lentiummmx <lentium.mmx@gmail.com>
2021-04-05 12:07:36 -0500

    j-td-by-example File(s) committed to adding improvements s3-19 (v0.1.0)

--- src/test/java/guru/springframework/MoneyTest.java
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -8,18 +8,18 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class MoneyTest {
     @Test
-    void testMultiplication() {
+    void testMultiplicationDollar() {
         Dollar five = new Dollar(5);
         Dollar product = five.times(2);
-        assertEquals(10, product.getAmount());
+        assertEquals(new Dollar(10), product);
 
         //testing side effects
         product = five.times(3);
-        assertEquals(15, product.getAmount());
+        assertEquals(new Dollar(15), product);
     }
 
     @Test
-    void testEquality() {
+    void testEqualityDollar() {
         assertEquals(new Dollar(5), new Dollar(5));
         assertNotEquals(new Dollar(5), new Dollar(8));
     }
